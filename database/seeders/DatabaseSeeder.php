ac2becb1bf4aaee3da29ec279134135a1eeca627
s-berdiyorov <berdiyorovsalokhiddin@gmail.com>
2021-12-05 19:34:44 +0100

    refactor: delete package.json file and update DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use App\Models\Category;
 use App\Models\Meal;
 use Illuminate\Database\Seeder;
@@ -15,7 +16,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(10)->create();
         Category::factory(10)->create();
         Meal::factory(20)->create();
     }
