e01e03330b5c269bd5005732a3163c22dda3d817
s-berdiyorov <berdiyorovsalokhiddin@gmail.com>
2021-12-14 23:55:53 +0100

    feat: add redirect with alert method

---
+++ app/Http/Controllers/Dashboard/BaseController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\RedirectResponse;
+
+class BaseController extends Controller
+{
+    /**
+     * Redirect after an action with alert
+     *
+     * @param $result
+     * @param $route
+     * @param $action
+     * @param string $type
+     * @return RedirectResponse
+     */
+    protected function redirectWithAlert($result, $route, $action, string $type = 'success'): RedirectResponse
+    {
+        $message = match ($action) {
+            "create" => 'Successfully saved',
+            "update" => 'Successfully updated',
+            "delete" => 'Successfully deleted',
+        };
+
+        if ($result) {
+            return redirect()
+                ->route('admin.' . $route . '.index')
+                ->with([$type => $message]);
+        } else {
+            return back()
+                ->with(['error' => 'Error, please contact your administrator'])
+                ->withInput();
+        }
+    }
+}
